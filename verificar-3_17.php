dc871eadc2efc8835924489155fbe9e0565d28df
AngelaGS2 <guijarrosuarezangela@gmail.com>
2025-10-27 12:38:46 +0100

    modificar errores 3_17

--- verificar-3_17.php
+++ verificar-3_17.php
@@ -1,20 +1,20 @@
 <?php
-include "usuarios.php";
+include "usuarios-3_17.php";
 
 $usuario = $_POST['usuario'];
 $clave = $_POST['clave'];
 
-$archivo = fopen("accesos.txt", "a");
+$archivo = fopen("accesos-3_17.txt", "a");
 
 if (isset($usuarios[$usuario]) && $usuarios[$usuario] == $clave) {
     fwrite($archivo, "El usuario $usuario ha accedido al sistema.\n");
     fclose($archivo);
-    header("Location: ok.php");
+    header("Location: ok-3_17.php");
     exit;
 } else {
     fwrite($archivo, "Intento fallido de acceso del usuario $usuario.\n");
     fclose($archivo);
-    header("Location: error.html");
+    header("Location: error-3_17.html");
     exit;
 }
 ?>
