9a6b4c76ed0ec6eb57ec180a23d9e77bbca81619
AngelaGS2 <161337979+AngelaGS2@users.noreply.github.com>
2025-10-27 13:11:33 +0100

    codigo index.php

---
+++ 3_20/index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hemeroteca</title>
+</head>
+<body>
+    <h1>Bienvenido a la Hemeroteca</h1>
+    <p>Selecciona una opción:</p>
+    <ul>
+        <li><a href="subir.php">Subir un archivo</a></li>
+        <li><a href="listar.php">Listar archivos</a></li>
+    </ul>
+</body>
+</html>
